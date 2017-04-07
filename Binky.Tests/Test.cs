9fcc1ac1300ead39293ddaa02a75d5cfea12a324
quezlatch <quezlatch@gmail.com>
2017-04-07 22:03:10 +0100

    fill out solution

---
+++ Binky.Tests/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit;
+namespace Binky.Tests
+{
+	public class Test
+	{
+		[Fact()]
+		public void TestMethod()
+		{
+		}
+	}
+}
---
+++ Binky/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Binky
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
