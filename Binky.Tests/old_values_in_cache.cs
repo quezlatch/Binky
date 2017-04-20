a3819833d77bb5464f0994bf403ef7ae74d9c87a
Michael Dickens <quezlatch@gmail.com>
2017-04-20 20:25:04 +0100

    really fix test

--- Binky.Tests/old_values_in_cache.cs
+++ Binky.Tests/old_values_in_cache.cs
@@ -6,7 +6,10 @@ namespace Binky.Tests
 {
 	public class old_values_in_cache : IDisposable
 	{
-		readonly Cache<string, DateTime> _cache;
+        const int EnoughTimeToPreload = 100;
+        const int TimeForAFewRefreshes = 1000;
+        const int TwoPreloadsAndOneRefresh = 3;
+        readonly Cache<string, DateTime> _cache;
 		int _count;
 
 		public old_values_in_cache()
@@ -26,9 +29,10 @@ namespace Binky.Tests
 		[Fact]
 		public void can_be_evicted_if_they_have_not_been_retrieved_since_the_last_refresh()
 		{
-			_cache.Get("a");
-			Thread.Sleep(1000);
-			Assert.Equal(1, _count);
+            Thread.Sleep(EnoughTimeToPreload);
+            _cache.Get("a");
+			Thread.Sleep(TimeForAFewRefreshes);
+			Assert.Equal(TwoPreloadsAndOneRefresh, _count);
 		}
 
 		public void Dispose()
